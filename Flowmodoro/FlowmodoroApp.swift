7874bb87de4f36661abc17bdf351568addbc26d1
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2024-03-06 19:59:23 -0500

    Initial Commit

---
+++ Flowmodoro/FlowmodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowmodoroApp.swift
+//  Flowmodoro
+//
+//  Created by Nicholas Fasching on 3/6/24.
+//
+
+import SwiftUI
+
+@main
+struct FlowmodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
